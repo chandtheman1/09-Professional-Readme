82eb09f784464967505c052868756fd27f11c99d
Chandler Liang <chandler.liang123@gmail.com>
2021-07-24 21:58:17 +1000

    updating the questions array

--- Develop/index.js
+++ Develop/index.js
@@ -4,47 +4,73 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
-    ["What is your GitHub Username?", "username"],
-    ["What is your email address?", "email"],
-    ["What is your project name?", "projectName"],
-    ["Please write a short description of your project?", "description"],
-    ["What kind of license should your project have?", "license"],
-    ["What command should be run to install dependencies?", "dependencies"],
-    ["what command should be run to run tests?", "test"],
-    ["What does the user need to know about using the repo?", "feature"],
-    ["What does the user need to know about contributing to the repo?", "contribution"],
+    { 
+        type: 'input',
+        name: 'username',
+        message: "What is your GitHub Username?",
+    },
+    { 
+        type: 'input',
+        name: 'email',
+        message: "What is your email address?",
+    },
+    { 
+        type: 'input',
+        name: 'description',
+        message: "Please write a short description of your project?",
+    },
+    { 
+        type: 'input',
+        name: 'license',
+        message: "What kind of license should your project have?",
+    },
+    { 
+        type: 'input',
+        name: 'dependencies',
+        message: "What command should be run to install dependencies?",
+    },
+    { 
+        type: 'input',
+        name: 'test',
+        message: "What command should be run to run tests?",
+    },
+    { 
+        type: 'input',
+        name: 'feature',
+        message: "What does the user need to know about using the repo?",
+    },
+    { 
+        type: 'input',
+        name: 'contribution',
+        message: "What does the user need to know about contributing to the repo?",
+    },
 ];
 
-questions.forEach(([question, name]) => {
-    questionObject = {
-        type: 'input',
-        message: question,
-        name: name,
-    }
-})
-
-console.log(questionObject);
+const userInputs = ["hello"];
 
 function inquireQuestion(questions) {
     inquirer
-        .prompt([ 
-            {
-                type: 'input',
-                message: question,
-                name: name,
-            }
-            
-        ])
+        .prompt(questions)
+        .then((data) => {
+            userInputs.push(data);
+        }
+        )
     
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        
+        err ? console.error(err) : console.log('Commit logged!');
+    })
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquireQuestion(questions);
-   
+    writeToFile('README.md', userInputs);
+    
 }
 
 // Function call to initialize app
