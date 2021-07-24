208ba60c92b4fc817e782431b1b49e83165588b3
Chandler Liang <chandler.liang123@gmail.com>
2021-07-24 20:05:15 +1000

    creating inquirer questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,23 +4,47 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is your GitHub Username?",
-    "What is your email address?",
-    "What is your project name?",
-    "Please write a short description of your project?",
-    "What kind of license should your project have?",
-    "What command should be run to install dependencies?",
-    "what command should be run to run tests?",
-    "What does the user need to know about using the repo?",
-    "What does the user need to know about contributing to the repo?",
+    ["What is your GitHub Username?", "username"],
+    ["What is your email address?", "email"],
+    ["What is your project name?", "projectName"],
+    ["Please write a short description of your project?", "description"],
+    ["What kind of license should your project have?", "license"],
+    ["What command should be run to install dependencies?", "dependencies"],
+    ["what command should be run to run tests?", "test"],
+    ["What does the user need to know about using the repo?", "feature"],
+    ["What does the user need to know about contributing to the repo?", "contribution"],
 ];
+
+questions.forEach(([question, name]) => {
+    questionObject = {
+        type: 'input',
+        message: question,
+        name: name,
+    }
+})
+
+console.log(questionObject);
+
+function inquireQuestion(questions) {
+    inquirer
+        .prompt([ 
+            {
+                type: 'input',
+                message: question,
+                name: name,
+            }
+            
+        ])
+    
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    generateMarkdown.consoleLog();
+    inquireQuestion(questions);
+   
 }
 
 // Function call to initialize app
